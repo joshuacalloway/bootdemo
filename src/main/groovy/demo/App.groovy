ea8d641d1b8d3542847523970c379da578df673a
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-18 09:35:19 -0500

    initial

---
+++ src/main/groovy/demo/App.groovy
@@ -0,0 +1,15 @@
+package demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by jc on 2/23/16.
+ */
+@SpringBootApplication
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
