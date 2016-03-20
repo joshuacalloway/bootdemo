e9ad41b2169ce8fc5520ed5dec7c799a8da0e6fc
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-20 18:00:50 -0500

    remove key

--- demo.sh
+++ demo.sh
@@ -1,1 +1,4 @@
-docker run -e TF_VAR_appname=bootdemo -v ~/.aws:/root/.aws bootdemo
+
+rm -f ~/.aws/bootdemo.pem
+docker run -v ~/.aws:/root/.aws bootdemo
+#docker run -e TF_VAR_appname=bootdemo -v ~/.aws:/root/.aws bootdemo
