d3327ada36b0ba08e552a71b833e60e1d5cb54d3
Joshua Calloway <joshua.calloway@gmail.com>
2016-03-20 22:50:41 -0500

    awsboot takes an arg

--- demo.sh
+++ demo.sh
@@ -1,4 +1,3 @@
 
 rm -f ~/.aws/bootdemo.pem
-docker run -v ~/.aws:/root/.aws bootdemo
-#docker run -e TF_VAR_appname=bootdemo -v ~/.aws:/root/.aws bootdemo
+docker run -v ~/.aws:/root/.aws bootdemo $1
